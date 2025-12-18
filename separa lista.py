523cd7b200cd622a889ce1ca2cec33eb3d2961fb
Rafael <rafael@gmail.com>
2025-12-17 22:52:50 -0300

    projetos em python

---
+++ separa lista.py
@@ -0,0 +1,17 @@
+lista= list()
+pares = list()
+impares = list()
+while True:
+    numero = int(input('digite um numero: '))
+    lista.append(numero)
+    resp=str(input('quer continuar? [s/n] ')).upper()
+    if resp=='N':
+        break
+for pos,v in enumerate(lista):
+    if v%2==0:
+        pares.append(v)
+    else:
+        impares.append(v)
+print(f' a lista completa é {lista}')
+print(f'a lista de pare é {pares}')
+print(f'a lista de impare {impares}')
